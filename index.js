f3b31b1292c38e8d9d7df4bba3352e5b5bef6241
liam <Liamjmiller.15@gmail.com>
2023-05-11 02:28:31 +0100

    pages full styles

--- index.js
+++ index.js
@@ -1,65 +1,19 @@
-// image gallery rotations
-// const sliderImg = document.getElementsByClassName("slider__imgs");
-// const sliderImgClass = "slider__img";
-
-// const slides = [
-//   "img1",
-//   "img2",
-//   "img3",
-//   "img4",
-//   "img5",
-//   "img6",
-//   "img7",
-//   "img8",
-//   "img9",
-//   "img10",
-//   "img11",
-//   "img12",
-//   "img13",
-//   "img14",
-//   "img15",
-//   "img16",
-//   "img17",
-//   "img18",
-//   "img19",
-//   "img20",
-//   "img21",
-//   "img22",
-//   "img23",
-// ];
-// console.log(sliderImg);
-
-// function slider() {
-//   for (i = 0; i < slides.length; i = i + 1) {
-//     // let nextImg =
-//     //   (sliderImg.innerHTML = `<img class="slider__img" src="${slides[i]}">`);
-//     setInterval(nextImg), 2000;
-//   }
-
-//   // sliderImg.innerHTML ="<img class=" + sliderImgClass + " src=" + slides[i] + ">";
-// }
-// slider();
-
-// // console.log(slider);
-
 // nav link change
+// const addClickEventHandlers = () => {
+//   const showRows = document.querySelectorAll(".nav__link");
 
-const addClickEventHandlers = () => {
-  const showRows = document.querySelectorAll(".nav__link");
+//   showRows.forEach((row) => {
+//     row.addEventListener("click", (e) => {
+//       const activeClass = "show--active";
 
-  showRows.forEach((row) => {
-    row.addEventListener("click", (e) => {
-      const activeClass = "show--active";
+//       const previouslyActiveRow = document.querySelector(`.${activeClass}`);
 
-      const previouslyActiveRow = document.querySelector(`.${activeClass}`);
+//       if (previouslyActiveRow) {
+//         previouslyActiveRow.classList.remove(activeClass);
+//       }
 
-      if (previouslyActiveRow) {
-        previouslyActiveRow.classList.remove(activeClass);
-      }
-
-      e.currentTarget.classList.add(activeClass);
-    });
-  });
-};
-
-addClickEventHandlers();
+//       e.currentTarget.classList.add(activeClass);
+//     });
+//   });
+// };
+// addClickEventHandlers();
