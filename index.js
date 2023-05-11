44b9437a60600494c52cb61e61f750c5a57f2da8
liam <Liamjmiller.15@gmail.com>
2023-05-11 04:06:43 +0100

    inital Splide Api use

--- index.js
+++ index.js
@@ -17,3 +17,32 @@
 //   });
 // };
 // addClickEventHandlers();
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var splide = new Splide(".splide");
+//   splide.mount();
+// });
+
+new Splide(".splide").mount();
+
+const splide = new Splide(".splide", {
+  type: "slide",
+  perPage: 3,
+  focus: "center",
+  omitEnd: true,
+  trimSpace: true,
+  width: "100%",
+  fixedHeight: "40rem",
+  cover: true,
+  breakpoints: {
+    640: {
+      perPage: 1,
+    },
+  },
+});
+
+splide.mount();
+
+// /|\
+//  |
+//dom the html for this once page issue is fixed
